98149aaf049f0d941d3dcfafebf00bc119d410b7
Lisbeth Ortega <118750598+lisbeth-op@users.noreply.github.com>
2023-02-06 13:13:09 -0400

    Add files via upload

---
+++ EjercicioCap6y7/EjercicioCapitulo6/EjercicioCapitulo6/Program.cs
@@ -0,0 +1,74 @@
+﻿
+int[][] calificaciones = new int[2][];
+
+
+
+
+
+
+for (int i = 0; i < 2; i++)
+{
+    Console.Write("Cuantos tiene la clase " + (i + 1) + ": ");
+    int numAlumnos = int.Parse(Console.ReadLine());
+    calificaciones[i] = new int[numAlumnos];
+    for (int j = 0; j < numAlumnos; j++)
+    {
+        Console.Write("Escriba  la calificacion del alumno " + (j + 1) + ": ");
+        calificaciones[i][j] = int.Parse(Console.ReadLine());
+    }
+}
+
+
+
+Console.Clear();
+Imprime(calificaciones);
+
+Console.WriteLine("\n\nEl promedio de todas las clases es: " + Promedio(calificaciones));
+
+
+Console.WriteLine("\nLa calificacion menor es: " + Menor(calificaciones));
+
+
+
+static void Imprime(int[][] arreglo)
+{
+  
+    for (int i = 0; i < 2; i++)
+    {
+        Console.WriteLine("Clase " + (i + 1));
+        for (int j = 0; j < arreglo[i].Length; j++)
+        {
+            Console.WriteLine("La calificcion del alumno " + (j + 1) + " es: " + arreglo[i][j]);
+        }
+    }
+}
+
+
+static double Promedio(int[][] arreglo)
+{
+   
+    double suma = 0.0;
+    int total = 0;
+    for (int i = 0; i < 2; i++)
+    {
+        for ( int j = 0; j < arreglo[i].Length; j++)
+        {
+            suma += arreglo[i][j];
+            total++;
+        }
+    }
+    return suma / total;
+}
+static int Menor(int[][] arreglo)
+{
+    int menor = int.MaxValue;
+    for (int i = 0; i < arreglo.Length; i++)
+    {
+        for (int j = 0; j < arreglo[i].Length; j++)
+        {
+            if (arreglo[i][j] < menor)
+                menor = arreglo[i][j];
+        }
+    }
+    return menor;
+}
---
+++ EjercicioCap6y7/ejercicio1Cap7/ejercicio1Cap7/Program.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+
+
+ArrayList calificaciones = new ArrayList();
+
+
+int numEstudiantes = 0;
+string calif = "";
+
+Console.WriteLine("Escriba la cantidad de estudiantes de la clase");
+numEstudiantes=int.Parse(Console.ReadLine());
+for (int i = 0; i < numEstudiantes; i++)
+{
+    Console.WriteLine("Ingrese la calificación del estudiante " + (i + 1));
+    calif = Console.ReadLine();
+    calificaciones.Add(Convert.ToInt32(calif));
+}
+
+Console.WriteLine("El promedio de las calificaciones es: " + Promedio(calificaciones));
+
+Console.WriteLine("La calificación mas baja es: " + calificacionMAsBaja(calificaciones));
+
+Console.WriteLine("La calificación mas alta es: " + CalificacionMasAlta(calificaciones));
+
+
+static double Promedio(ArrayList calificaciones)
+{
+    double suma = 0.0;
+
+    for (int i = 0; i < calificaciones.Count; i++)
+    {
+        suma += (int)calificaciones[i];
+    }
+
+    return suma / calificaciones.Count;
+}
+
+static int calificacionMAsBaja(ArrayList calificaciones)
+{
+    int CalifBaj = (int)calificaciones[0];
+
+    for (int i = 1; i < calificaciones.Count; i++)
+    {
+        if ((int)calificaciones[i] < CalifBaj)
+        {
+            CalifBaj = (int)calificaciones[i];
+        }
+    }
+
+    return CalifBaj;
+}
+
+static int CalificacionMasAlta(ArrayList calificaciones)
+{
+    int CalifAlt = (int)calificaciones[0];
+
+    for (int i = 1; i < calificaciones.Count; i++)
+    {
+        if ((int)calificaciones[i] > CalifAlt)
+        {
+            CalifAlt = (int)calificaciones[i];
+        }
+    }
+
+    return CalifAlt;
+}
